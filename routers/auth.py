b4ea3450529adba781dc3dc162a1399f9e077c4e
hooman <hooman.m2048@gmail.com>
2023-11-03 23:46:51 +0330

    added role in jwt

--- routers/auth.py
+++ routers/auth.py
@@ -11,7 +11,6 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 from datetime import timedelta, datetime
 
-
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
@@ -51,10 +50,11 @@ def authenticate_user(db: db_dependency, username: str, password: str):
     return user
 
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
     encode = {
         'sub': username,
-        'id': user_id
+        'id': user_id,
+        'role': role
     }
     expires = datetime.utcnow() + expires_delta
     encode.update({'exp': expires})
@@ -66,9 +66,10 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: str = payload.get('id')
+        user_role: str = payload.get('role')
         if username is None or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='could not validate user')
-        return {'username': username, 'id': user_id}
+        return {'username': username, 'id': user_id, 'user_role': user_role}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='could not validate user')
 
@@ -91,5 +92,5 @@ async def get_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='authentication failed')
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
     return {'access_token': token, 'token_type': 'bearer'}
