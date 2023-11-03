8642336c08ab308ede0aec3b9684d63b6bd46b2f
hooman <hooman.m2048@gmail.com>
2023-11-03 23:47:07 +0330

    add admin router

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
 from database import Base, engine
-from routers import todo, auth
+from routers import todo, auth, admin
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 app.include_router(todo.router)
 app.include_router(auth.router)
+app.include_router(admin.router)
